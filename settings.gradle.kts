4b9008ebab215d17d754346105eefb076bfc1f94
Silvio Giebl <silvio.giebl@hivemq.com>
2023-10-15 23:25:24 +0200

    Add version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,1 @@
 rootProject.name = "hivemq-sparkplug-extension"
-
-pluginManagement {
-    plugins {
-        id("com.hivemq.extension") version "${extra["plugin.hivemq-extension.version"]}"
-        id("com.google.protobuf") version "${extra["plugin.protobuf.version"]}"
-        id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-        id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
-        id("org.asciidoctor.jvm.convert") version "${extra["plugin.asciidoctor.version"]}"
-    }
-}
